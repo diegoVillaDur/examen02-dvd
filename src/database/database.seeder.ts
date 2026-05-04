91d17d2725972920da89e24650c0d79b952061c8
diegoVillaDur <164115110+diegoVillaDur@users.noreply.github.com>
2026-05-03 21:55:14 -0600

    asdasd

--- src/database/database.seeder.ts
+++ src/database/database.seeder.ts
@@ -1,7 +1,7 @@
 import { Injectable, OnModuleInit, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import { User } from '../users/user.entity';
 import { Role } from '../common/enums/role.enum';
 
